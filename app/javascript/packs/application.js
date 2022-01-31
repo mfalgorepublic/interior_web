eb3cd15e470156a3297633d3ff21887e64f78a58
mfalgorepublic <mfurqan@algorepublic.com>
2022-01-31 15:42:26 +0500

    theme

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,38 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
+// import "jquery"
+// import {} from 'jquery-ujs'
+// import "bootstrap"
+
+// import "main"
+// require("main")
+require("stylesheets/application.scss")
+
+// require("assets/js/main.js")
+// require("assets/vendor/bootstrap/js/bootstrap.bundle.min.js")
+// require ("HeroBiz/assets/vendor/aos/aos.js")
+// require ("HeroBiz/assets/vendor/glightbox/js/glightbox.min.js")
+// require ("HeroBiz/assets/vendor/isotope-layout/isotope.pkgd.min.js")
+// require ("HeroBiz/assets/vendor/swiper/swiper-bundle.min.js")
+// require ("HeroBiz/assets/vendor/php-email-form/validate.js")
+//   // <!-- Template Main JS File -->
+// require ("HeroBiz/assets/js/main.js")
+
+
+
